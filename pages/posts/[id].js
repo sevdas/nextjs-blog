c7cb9ede109085bd874b468b13128c96414c41d9
sevdas <sshukrieva@gmail.com>
2021-05-17 17:04:55 +0100

    first commit
    Co-authored-by: Evgeny Pletnev <e.a.pletnev@gmail.com>

---
+++ pages/posts/[id].js
@@ -0,0 +1,45 @@
+import Layout from "../../components/layout";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+// Use `getStaticProps` to fetch a specific post given an ID and `getStaticPaths` to fetch all possible blog posts.
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+  // ...
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
